038b0b29d29e4b20187ba8d5e63caa729b8a1c5e
Ladislav Lučivjanský <l.lucivjansky@gmail.com>
2023-12-01 11:27:27 +0100

    Update day1.py

--- day1/day1.py
+++ day1/day1.py
@@ -1,1 +1,10 @@
-
+with open("day1.txt", "r+") as file:
+    input = file.read().split("\n")
+val_list = []
+for row in input:
+  numbers = []
+    for char in row:
+      if char.isnumeric
+        numbers.append(char)
+    val_list.append(numbers[0]numbers[-1])
+print("Sum of calib. values", sum(val_list))
