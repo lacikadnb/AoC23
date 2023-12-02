36fb4304118e791bbad99ac0b1f7ce63d87182ec
lacika_dnb <l.lucivjansky@gmail.com>
2023-12-02 22:14:28 +0100

    day2, part 2 done

--- day2/cube_conundrum.py
+++ day2/cube_conundrum.py
@@ -1,22 +1,31 @@
 import re
+import numpy as np
 
 with open("input.txt", "r+") as file:
     data_input = file.read().split("\n")
 
 # game data processing
 game_results = {}
+min_cubes = []
 for row in data_input:
     game_id = int(re.search(r"\d+", row).group())
     game_data = row[row.index(":")+1:]
     results = []
     groups = game_data.split(';')
+    row_buffer = {}
     for group in groups:
         result = {}
         matches = re.findall(r'(\d+)\s*([a-zA-Z]+)', group)
         for match in matches:
             number, word = match
             result[word.strip()] = int(number)
+            try:
+                if row_buffer[word.strip()] < int(number):
+                    row_buffer[word.strip()] = int(number)
+            except (KeyError, TypeError):
+                row_buffer[word.strip()] = int(number)
         results.append(result)
+    min_cubes.append(row_buffer)
     game_results[game_id] = results
 
 # game runs approximation
@@ -32,4 +41,10 @@ for game in game_results.items():
                 current_run.append(0)
     if all(current_run):
         valid_runs.append(game[0])
-print(sum(valid_runs))
+print(f'Sum of the IDs of valid games is: {sum(valid_runs)}')
+
+# min cubes for successful run
+powers = []
+for run in min_cubes:
+    powers.append(np.prod(list(run.values())))
+print(f'Sum of the power of valid sets is: {sum(powers)}')
