756fdad7fbc86927fdac151c508c3f6356dbbba2
lacika_dnb <l.lucivjansky@gmail.com>
2023-12-03 03:35:52 +0100

    code cleanup

--- day2/cube_conundrum.py
+++ day2/cube_conundrum.py
@@ -6,45 +6,28 @@ with open("input.txt", "r+") as file:
 
 # game data processing
 game_results = {}
-min_cubes = []
 for row in data_input:
     game_id = int(re.search(r"\d+", row).group())
-    game_data = row[row.index(":")+1:]
-    results = []
-    groups = game_data.split(';')
-    row_buffer = {}
+    groups = row[row.index(":")+1:].split(';')
+    row_buffer = {"red": 0, "green": 0, "blue": 0}
     for group in groups:
-        result = {}
         matches = re.findall(r'(\d+)\s*([a-zA-Z]+)', group)
         for match in matches:
             number, word = match
-            result[word.strip()] = int(number)
-            try:
-                if row_buffer[word.strip()] < int(number):
-                    row_buffer[word.strip()] = int(number)
-            except (KeyError, TypeError):
+            if row_buffer[word.strip()] < int(number):
                 row_buffer[word.strip()] = int(number)
-        results.append(result)
-    min_cubes.append(row_buffer)
-    game_results[game_id] = results
+    game_results[game_id] = row_buffer
 
 # game runs approximation
-run = {"red": 12, "green": 13, "blue": 14}
+limit = {"red": 12, "green": 13, "blue": 14}
 valid_runs = []
 for game in game_results.items():
-    current_run = []
-    for game_set in game[1]:
-        for color in game_set:
-            if game_set[color] <= run[color]:
-                current_run.append(1)
-            else:
-                current_run.append(0)
-    if all(current_run):
+    if game[1]["red"] <= limit["red"] and game[1]["green"] <= limit["green"] and game[1]["blue"] <= limit["blue"]:
         valid_runs.append(game[0])
 print(f'Sum of the IDs of valid games is: {sum(valid_runs)}')
 
 # min cubes for successful run
 powers = []
-for run in min_cubes:
+for run in list(game_results.values()):
     powers.append(np.prod(list(run.values())))
 print(f'Sum of the power of valid sets is: {sum(powers)}')
