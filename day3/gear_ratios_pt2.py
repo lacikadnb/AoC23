27e9b9be8dc3a670475a1c45b8ae4742e253c4b0
lacika_dnb <l.lucivjansky@gmail.com>
2023-12-03 22:04:14 +0100

    Day 3, part 2 done

---
+++ day3/gear_ratios_pt2.py
@@ -0,0 +1,46 @@
+import re
+
+with open("input.txt", "r+") as file:
+    data_input = file.read().split("\n")
+
+valid_gears = []
+for i, row in enumerate(data_input):
+    matches = re.finditer(r"\d+", row)
+    length = len(row)
+    for match in matches:
+        start_col_index = match.start()
+        end_col_index = match.end()
+        number = match.group()
+        index = [start_col_index, end_col_index]
+        for j in range(int(index[0])-1, int(index[1])+1):
+            # Check number in actual row
+            if i > 0 and j < length:
+                if data_input[i][j] == "*" and not data_input[i][j].isnumeric():
+                    valid_gears.append([int(number), (i, j)])
+                    break
+            # Check number in upper row
+            if i > 0 and j < length:
+                if data_input[i-1][j] == "*" and not data_input[i-1][j].isnumeric():
+                    valid_gears.append([int(number), (i-1, j)])
+                    break
+            # Check number in actual lower row
+            if i < length - 1 and j < length:
+                if data_input[i+1][j] == "*" and not data_input[i+1][j].isnumeric():
+                    valid_gears.append([int(number), (i+1, j)])
+                    break
+
+# filter non-unique coordinates
+tuple_frequency = {}
+for item in valid_gears:
+    _, tpl = item
+    tuple_frequency[tpl] = tuple_frequency.get(tpl, 0) + 1
+filtered_data = [item for item in valid_gears if tuple_frequency[item[1]] > 1]
+
+# multiply values with same coordinates
+couples = {}
+for gear in filtered_data:
+    if couples.get(gear[1]) is not None:
+        couples[gear[1]] *= gear[0]
+    else:
+        couples[gear[1]] = gear[0]
+print(f'Sum of all of the part numbers in the engine schematic is {sum(list(couples.values()))}')
