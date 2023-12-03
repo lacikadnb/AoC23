162bca6c36b643e75b5bd89edff3d88003050d5f
lacika_dnb <l.lucivjansky@gmail.com>
2023-12-03 03:04:41 +0100

    code cleanup

--- day1/trebuchet.py
+++ day1/trebuchet.py
@@ -7,18 +7,18 @@ with open("input.txt", "r+") as file:
 val_list = []
 for row in data_input:
     numbers = {}
-    # ------------------------------------------ Part 2 start --------------------------------------------------- #
+    # ------------------------------------------ Part 1 ---------------------------------------------------- #
+    # Scan for arabic numbers
+    for i, char in enumerate(row):
+        if char.isnumeric():
+            numbers[i] = char
+    # ------------------------------------------ Part 2 --------------------------------------------------- #
     # Scan for written numbers
     for k, v in string_digits.items():
         if re.search(k, row):
             indexes = [x.start(0) for x in re.finditer(k, row)]
             for i in indexes:
                 numbers[i] = str(v)
-    # ------------------------------------------ Part 2 end ---------------------------------------------------- #
-    # Scan for arabic numbers
-    for i, char in enumerate(row):
-        if char.isnumeric():
-            numbers[i] = char
     sorted_numbers = sorted(numbers.items())
     val_list.append(int(f'{sorted_numbers[0][1]}{sorted_numbers[-1][1]}'))
 print("Sum of calib. values", sum(val_list))
