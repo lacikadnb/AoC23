842a0921b5d27f64f0524e759ec175963638fde6
lacika_dnb <l.lucivjansky@gmail.com>
2023-12-06 19:14:03 +0100

    Day 6, deleted redundant code

--- day6/wait_for_it.py
+++ day6/wait_for_it.py
@@ -3,33 +3,18 @@ import numpy as np
 
 with open("input.txt", "r+") as file:
     data_input = file.read().split("\n")
-    merged_input = [re.sub(" ", "", row) for row in data_input]
 
 times = [int(x) for x in re.findall(r"\d+", data_input[0])]
 distances = [int(x) for x in re.findall(r"\d+", data_input[1])]
-merged_time = [int(x) for x in re.findall(r"\d+", merged_input[0])]
-merged_distance = [int(x) for x in re.findall(r"\d+", merged_input[1])]
+merged_time = [int(''.join(map(str, times)))]
+merged_distance = [int(''.join(map(str, distances)))]
 
 
 def race(tm, dst):
-    results = []
-    for i, time in enumerate(tm):
-        run = []
-        for t in range(0, time+1):
-            speed = t*1
-            duration = time-t
-            result = speed*duration
-            run.append(result)
-        results.append(run)
-
     wins = []
-    for i, run in enumerate(results):
-        d = dst[i]
-        win = 0
-        for r in run:
-            if r > d:
-                win += 1
-        wins.append(win)
+    for i, time in enumerate(tm):
+        run = [r*(time-r) for r in range(0, time+1)]
+        wins.append(len(list(filter(lambda r: r > dst[i], run))))
     return np.prod(wins)
 
 
