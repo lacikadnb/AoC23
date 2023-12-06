68e138c82f8b5599f7bff861baa9b1b9aa848f2f
lacika_dnb <l.lucivjansky@gmail.com>
2023-12-06 20:22:49 +0100

    Day 6, def moved up

--- day6/wait_for_it.py
+++ day6/wait_for_it.py
@@ -1,14 +1,6 @@
 import re
 import numpy as np
 
-with open("input.txt", "r+") as file:
-    data_input = file.read().split("\n")
-
-times = [int(x) for x in re.findall(r"\d+", data_input[0])]
-distances = [int(x) for x in re.findall(r"\d+", data_input[1])]
-merged_time = [int(''.join(map(str, times)))]
-merged_distance = [int(''.join(map(str, distances)))]
-
 
 def race(tm, dst):
     wins = []
@@ -17,6 +9,14 @@ def race(tm, dst):
         wins.append(len(list(filter(lambda r: r > dst[i], run))))
     return np.prod(wins)
 
+
+with open("input.txt", "r+") as file:
+    data_input = file.read().split("\n")
+
+times = [int(x) for x in re.findall(r"\d+", data_input[0])]
+distances = [int(x) for x in re.findall(r"\d+", data_input[1])]
+merged_time = [int(''.join(map(str, times)))]
+merged_distance = [int(''.join(map(str, distances)))]
 
 print(f"Part 1: {race(times, distances)}")
 print(f"Part 2: {race(merged_time, merged_distance)}")
